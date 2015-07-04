f1e1647f23011102df31a8cd7e19d0df88097dc4
s4t4r0th <sbessa313@gmail.com>
2015-07-04 00:23:45 -0300

    Changed main banner and added necessary dependencies to gemspec.

--- lib/jurandir/common.rb
+++ lib/jurandir/common.rb
@@ -73,38 +73,28 @@ module Jurandir
   end
   
   def Jurandir.banner
-    system 'cls'
-    print "\n".red.bold
-    print "   ___                           _ _         ___      _           _        ______ _           _           \n".red.bold
-    print "  |_  |                         | (_)       / _ \\    | |         (_)       |  ___(_)         | |          \n".red.bold
-    print "    | |_   _ _ __ __ _ _ __   __| |_ _ __  / /_\\ \\ __| |_ __ ___  _ _ __   | |_   _ _ __   __| | ___ _ __ \n".red.bold
-    print "    | | | | | '__/ _` | '_ \\ / _` | | '__| |  _  |/ _` | '_ ` _ \\| | '_ \\  |  _| | | '_ \\ / _` |/ _ \\ '__|\n".red.bold
-    print "/\\__/ / |_| | | | (_| | | | | (_| | | |    | | | | (_| | | | | | | | | | | | |   | | | | | (_| |  __/ |   \n".red.bold
-    print "\\____/ \\__,_|_|  \\__,_|_| |_|\\__,_|_|_|    \\_| |_/\\__,_|_| |_| |_|_|_| |_| \\_|   |_|_| |_|\\__,_|\\___|_|   \n".red.bold
-    print "                   .:/+ooo++/-.                   \n".red.bold
-    print "              -+ooo/-.`   ``.:+ooo/.              +------------------------------------------------------+\n".red.bold
-    print "           -ss/`                  -oso.           | .:jurandir.rb, the perfect admin panel finder v1.7:. |\n".red.bold
-    print "         /h+`                      -oyMh-         |        .:Coded By SuperSenpai & PrestusHood:.        |\n".red.bold
-    print "       :domd/`                   .-.yNMssy.       |~+~Sponsored by Lolicon Squad & ProtoWave Reloaded~+~ |\n".red.bold
-    print "      sMd  +NN+                    `MNNN .d/      |                                                      |\n".red.bold
-    print "    `hsMh   /M+                    -s/MN  `h+     |                     <:~Contact:~>                    |\n".red.bold
-    print "    h+ oN`   ++`                   `oNN+    d/    |                                                      |\n".red.bold
-    print "   +h   /h-          `           -.yMo`     .N`   | ___ ___     -------------------+---------------------|\n".red.bold
-    print "   N:     --.`    `hMMy         sMMNM-       yo   || __| _ ) <> fb.com/protowave02 | fb.com/loliconsquad |\n".red.bold
-    print "  -M          :   -MMNs`        :yMhM-   `.``/d   || _|| _ \\                       |                     |\n".red.bold
-    print "  :m          d    -/.            ` M`.:-.   :m   ||_| |___/ <> fb.com/supersenpai | fb.com/prestushood1 |\n".red.bold
-    print "  -M          M/                   `d:.      /h   |             -------------------+---------------------|\n".red.bold
-    print "   m:         mN/                `.+:...--::-d+   |                                                      |\n".red.bold
-    print "   +d.-----:--:hMh..````           :        .m`   |                                                      |\n".red.bold
-    print "    ho     .---.:yd/.`            -/-:/+:---m:    |                                                      |\n".red.bold
-    print "    `ho-:::.---....//:-   `oyNdh`:h.-` `-::d/     |                                                      |\n".red.bold
-    print "      sy`:::`.----.. -/s:` .hNmyhh--`-::`-d:      |                                                      |\n".red.bold
-    print "       :d+.::../-  ::  `/hmNMdys/   :: .hs`       |                                                      |\n".red.bold
-    print "         /ho./-   /.                 -hs.         |                                                      |\n".red.bold
-    print "           -sy+. -               `:os+`           |                                                      |\n".red.bold
-    print "              .+sso+:-..``..-/oooo:`              |        〜(^∇^〜）Sejam vadias mágicas（〜^∇^)〜        |\n".red.bold
-    print "                   `-:/+++//:.`                   +------------------------------------------------------+\n".red.bold
+    system 'clear'
     print "\n"
+    print " _           _ _                   _____           _ _    _ _                   \n".bold.red
+    print "| |         | (_)                 |_   _|         | | |  (_) |  　     ∩＿＿＿∩  \n".bold.red
+    print "| |     ___ | |_  ___ ___  _ __     | | ___   ___ | | | ___| |_      |ノ  　  ヽ \n".bold.red
+    print "| |    / _ \\| | |/ __/ _ \\| '_ \\    | |/ _ \\ / _ \\| | |/ / | __|    /　 ●　  ●  |\n".bold.red
+    print "| |___| (_) | | | (_| (_) | | | |   | | (_) | (_) | |   <| | |_    |　 ( _●_)  ミ\n".bold.red
+    print "\\_____/\\___/|_|_|\\___\\___/|_| |_|   \\_/\\___/ \\___/|_|_|\\_\\_|\\__|  彡､  　 |∪| ､｀＼\n".bold.red
+    print "+------------------------------------------------------------------------------+\n".bold.red
+    print "|               .:lolicon.rb the perfect tookit for lazy hacking:.             |\n".bold.red
+    print "|                     .:Coded By SuperSenpai & PrestusHood:.                   |\n".bold.red
+    print "|             ~+~Sponsored by Lolicon Squad & ProtoWave Reloaded~+~            |\n".bold.red
+    print "|                                                                              |\n".bold.red
+    print "|                                 <:~Contact:~>                                |\n".bold.red
+    print "|                                                                              |\n".bold.red
+    print "||FB:/protowave02 |FB:/loliconsquad |FB:/PrestusHood1   |FB:id=100003199327670 |\n".bold.red
+    print "||TT:@Protowave01 |TT: N/A          |TT:@PrestusHood    |TT: N/A               |\n".bold.red
+    print "||YT: N/A         |YT: N/A          |YT:/PrestusHood    |YT: N/A               |\n".bold.red
+    print "||Steam: N/A      |Steam: N/A       |Steam:/PrestusHood |Steam:/SuperSenpai    |\n".bold.red
+    print "|                                                                              |\n".bold.red
+    print "|            Support us buying steam games and sejam vadias mágicas            |\n".bold.red
+    print "+------------------------------------------------------------------------------+\n".bold.red
   end
   
   def Jurandir.die(msg)
--- lolicon.gemspec
+++ lolicon.gemspec
@@ -16,4 +16,8 @@ Gem::Specification.new do |s|
   s.executables = s.files.grep(/^bin\//) { |file| File.basename(file) }
   s.test_files = s.files.grep(/^test\//)
   s.require_paths = ['lib']
+
+  ['colorize', 'google-search'].each { |dep|
+    s.add_runtime_dependency dep
+  }
 end
