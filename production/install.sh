a58a633e563a912c0311073002e2ea1e8cfd48a7
s4t4r0th <sbessa313@gmail.com>
2015-07-05 22:01:29 -0300

    Added production files.

---
+++ production/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Installing DoomCaster..."
+
+LISTS_PATH=$HOME/.doomcaster/wordlists
+
+if ! mkdir -p $LISTS_PATH; then
+	echo "FATAL: Failed to create lists directory"
+	exit 1
+fi
+
+if ! cp -r wordlists/* $LISTS_PATH; then
+	echo "FATAL: Failed to move wordlists to the lists path"
+	exit 1
+fi
+
+echo "List set up OK"
+echo "Installing gem..."
+
+gem install doomcaster*.gem
+
+echo "DoomCaster installed with success"
